ccde54900dd8f8ced3514243a58f18290e2a097b
SpencerJ <spencerj21@chcstudent.com>
2019-11-19 09:50:08 -0500

    generatePaths() now exists

---
+++ src/pathGen.cpp
@@ -0,0 +1,13 @@
+#include "robot.hpp"
+
+using namespace okapi::literals;
+
+void generatePaths(){
+    robot::chassisProfiler->generatePath({{0_in, 0_in, 0_deg}, {40_in, 0_in, 0_deg}}, "cubeAdvance", {0.6, 1.5, 10.0});
+    robot::chassisProfiler->storePath("/usd/tt/paths/cubeAdvance", "cubeAdvance");
+    robot::chassisProfiler->removePath("cubeAdvance");
+
+    robot::chassisProfiler->generatePath({{0_in, 0_in, 0_deg}, {48_in, 0_in, 0_deg}}, "smallZoneAdvance", {0.6, 0.8, 10.0});
+    robot::chassisProfiler->storePath("/usd/tt/paths/smallZoneAdvance", "smallZoneAdvance");
+    robot::chassisProfiler->removePath("smallZoneAdvance");
+}
