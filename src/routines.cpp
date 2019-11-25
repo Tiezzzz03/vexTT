4fcd8809fba751394e264f6da99c66e708f30b9f
SpencerJ <spencerj21@chcstudent.com>
2019-11-25 16:08:25 -0500

    comp tuning

--- src/routines.cpp
+++ src/routines.cpp
@@ -22,6 +22,22 @@ AutonomousRoutine(
 ),
 
 AutonomousRoutine(
+  "Cube Push",
+  &lv_style_plain,
+  [](screen::ttField *field){
+    field->finishDrawing();
+  },
+  [](){
+    robot::chassis->getModel()->setMaxVelocity(100);
+    robot::chassis->moveDistance(40_in);
+
+    robot::chassis->moveDistance(-12_in);
+    
+    robot::chassis->getModel()->setMaxVelocity(200);
+  }
+),
+
+AutonomousRoutine(
   "Small Zone Red",
   &screen::resources::redText,
   [](screen::ttField *field) {
