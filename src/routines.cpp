c7fc1bb5a575268e6679295a89db4fe5c650bc90
SpencerJ <spencerj21@chcstudent.com>
2019-12-12 08:19:04 -0500

    autonomous testing

--- src/routines.cpp
+++ src/routines.cpp
@@ -83,7 +83,7 @@ AutonomousRoutine(
 ),
 
 AutonomousRoutine(
-  "Large Zone Blue",
+  "Large Zone Red",
   &screen::resources::redText,
   [](screen::ttField *field) {},
   [](){
@@ -103,7 +103,52 @@ AutonomousRoutine(
     robot::chassisProfiler->waitUntilSettled();
 
     robot::chassis->getModel()->setMaxVelocity(60);
-    robot::chassis->turnAngleAsync(130_deg);
+    robot::chassis->turnAngleAsync(-126_deg);
+    robot::intake->moveVoltage(12000);
+    //robot::chassisProfiler->loadPath("/usd/tt/paths/smallZoneAdvance", "zoneAdvance"); //60% speed, low accel, ~48_in
+    robot::chassisProfiler->generatePath({{0_in, 0_in, 0_deg}, {40.5_in, 0_in, 0_deg}}, "zoneAdvance", {0.6, 0.8, 5.0});
+    robot::chassis->waitUntilSettled();
+    
+    robot::chassis->getModel()->setMaxVelocity(200);
+    robot::chassisProfiler->setTarget("zoneAdvance");
+    robot::angler->prime();
+    robot::chassisProfiler->waitUntilSettled();
+    robot::intake->moveVoltage(0);
+
+    robot::angler->stack();
+    pros::delay(100);
+    robot::angler->waitUntilSettled();
+
+    robot::chassis->getModel()->setMaxVelocity(80);
+    robot::chassis->moveDistance(-6_in);
+    robot::angler->prime();
+
+    robot::chassis->getModel()->setMaxVelocity(200);
+  }
+),
+
+AutonomousRoutine(
+  "Large Zone Blue",
+  &screen::resources::blueText,
+  [](screen::ttField *field) {},
+  [](){
+    robot::intake->moveVoltage(-12000);
+    pros::delay(500);
+
+    //robot::chassisProfiler->loadPath("/usd/tt/paths/cubeAdvance", "cubeAdvance"); //60% speed, low accel, ~40_in
+    robot::chassisProfiler->generatePath({{0_in, 0_in, 0_deg}, {36_in, 0_in, 0_deg}}, "cubeAdvance", {1, 2, 10});
+    robot::chassis->getModel()->setMaxVelocity(100);
+    robot::chassis->moveDistance(9_in);
+    robot::chassis->moveDistance(-4_in);
+
+    robot::chassis->getModel()->setMaxVelocity(200);
+    robot::angler->prime();
+    robot::chassisProfiler->setTarget("cubeAdvance");
+    robot::intake->moveVoltage(6000);
+    robot::chassisProfiler->waitUntilSettled();
+
+    robot::chassis->getModel()->setMaxVelocity(60);
+    robot::chassis->turnAngleAsync(126_deg);
     robot::intake->moveVoltage(12000);
     //robot::chassisProfiler->loadPath("/usd/tt/paths/smallZoneAdvance", "zoneAdvance"); //60% speed, low accel, ~48_in
     robot::chassisProfiler->generatePath({{0_in, 0_in, 0_deg}, {40.5_in, 0_in, 0_deg}}, "zoneAdvance", {0.6, 0.8, 5.0});
