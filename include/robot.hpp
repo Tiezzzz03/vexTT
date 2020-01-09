93841cc1557b6031f84e84c4fd58659570b1548a
SpencerJ <spencerj21@chcstudent.com>
2020-01-09 09:46:04 -0500

    wipe screenController

--- include/robot.hpp
+++ include/robot.hpp
@@ -4,11 +4,10 @@
 #include "mechanism/lift.hpp"
 
 enum class screenMode{
-  disabled, notification, selection, ez
+  disabled, notification, selection, ez, NUMPANELS
 };
 
 namespace robot {
-
 extern okapi::Controller controller;
 
 extern std::shared_ptr<Lift> lift;
@@ -20,10 +19,9 @@ extern std::shared_ptr<okapi::MotorGroup> rDrive;
 extern std::shared_ptr<okapi::ChassisController> chassis;
 extern std::shared_ptr<okapi::AsyncMotionProfileController> chassisProfiler;
 
-namespace screen{
+namespace screen {
   extern pros::Task *controller;
   extern volatile screenMode state;
   extern std::string notification;
-};
-
+}
 }
